1cd43eb775819fea970f97e2864926a4a7211b5c
Gabriel Palma <gubpalma@hotmail.com>
2025-08-01 14:49:53 +1000

    test: rounded out specification transmutability testing example

--- src/9.0/White.Knight.InMemory/Translator/InMemoryCommandTranslator.cs
+++ src/9.0/White.Knight.InMemory/Translator/InMemoryCommandTranslator.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using White.Knight.Domain;
 using White.Knight.Domain.Exceptions;
@@ -33,18 +34,25 @@ namespace White.Knight.InMemory.Translator
         {
             var specification = command.Specification;
 
-            if (specification is SpecificationThatIsNotCompatible<TD>)
-                throw new UnparsableSpecificationException();
-
-            var query = $"RUN QUERY OF {typeof(TD).Name.ToUpper()} FILTER BY X Y Z";
-
-            _logger
-                .LogDebug("Translated Query: ({specification}) [{query}]", specification.GetType().Name, query);
-
-            return new InMemoryTranslationResult
+            try
             {
-                ExampleQueryLanguageString = query
-            };
+                var query = $"RUN QUERY OF {typeof(TD).Name.ToUpper()} {Translate(specification)} FILTER BY X Y Z";
+
+                _logger
+                    .LogDebug("Translated Query: ({specification}) [{query}]", specification.GetType().Name, query);
+
+                return new InMemoryTranslationResult
+                {
+                    ExampleQueryLanguageString = query
+                };
+            }
+            catch (Exception e) when (e is NotImplementedException or UnparsableSpecificationException)
+            {
+                _logger
+                    .LogDebug("Error translating Query: ({specification})", specification.GetType().Name);
+
+                throw;
+            }
         }
 
         public InMemoryTranslationResult Translate(IUpdateCommand<TD> command)
@@ -57,6 +65,25 @@ namespace White.Knight.InMemory.Translator
             return new InMemoryTranslationResult
             {
                 ExampleQueryLanguageString = query
+            };
+        }
+
+        private string Translate(Specification<TD> spec)
+        {
+            return spec switch
+            {
+                SpecificationByAll<TD> => " TRUE ",
+                SpecificationByNone<TD> => " FALSE ",
+                SpecificationByEquals<TD, string> eq => $" {eq.Property.Name} = '{eq.Value}' ",
+                SpecificationByEquals<TD, int> eq => $" {eq.Property.Name} = {eq.Value} ",
+                SpecificationByEquals<TD, Guid> eq => $" {eq.Property.Name} = {eq.Value} ",
+                SpecificationByAnd<TD> and => $"( {Translate(and.Left)} AND {Translate(and.Right)} )",
+                SpecificationByOr<TD> or => $"( {Translate(or.Left)} AND {Translate(or.Right)} )",
+                SpecificationByNot<TD> not => $"NOT ( {Translate(not.Spec)} )",
+                SpecificationByTextStartsWith<TD> text => $" STARTSWITH {text.Value} )",
+                SpecificationByTextContains<TD> text => $" CONTAINS {text.Value} )",
+                SpecificationThatIsNotCompatible<TD> => throw new UnparsableSpecificationException(),
+                _ => throw new NotImplementedException()
             };
         }
     }
---
+++ src/9.0/White.Knight.Tests.Domain/Specifications/CustomerSpecByComplexQuery.cs
@@ -0,0 +1,8 @@
+﻿using White.Knight.Domain;
+
+namespace White.Knight.Tests.Domain.Specifications
+{
+    public class CustomerSpecByComplexQuery(string value)
+        : SpecificationByOr<Customer>(new SpecificationByEquals<Customer, string>(o => o.CustomerName, value),
+            new SpecificationByNot<Customer>(new SpecificationByEquals<Customer, int>(o => o.CustomerNumber, 400)));
+}
