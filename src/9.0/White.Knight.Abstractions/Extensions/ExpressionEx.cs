3b8d3cf04fdaee8df69f0023c1f972f21d25129f
Gabriel Palma <gubpalma@hotmail.com>
2025-08-11 18:12:04 +1000

    feat: added general extension method for property pathing

---
+++ src/9.0/White.Knight.Abstractions/Extensions/ExpressionEx.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+namespace White.Knight.Abstractions.Extensions
+{
+    public static class ExpressionEx
+    {
+        public static string GetPropertyExpressionPath(
+            ref string name,
+            Expression ex,
+            string separator = ".")
+        {
+            if (ex is MemberExpression memberExpression)
+            {
+                if (memberExpression.Expression is MemberExpression subMemberExpression)
+                {
+                    GetPropertyExpressionPath(ref name, subMemberExpression);
+                }
+
+                if (!string.IsNullOrEmpty(name))
+                    name += separator;
+
+                name +=
+                    memberExpression
+                        .Member
+                        .GetMemberPropertyOrJsonAlias();
+            }
+            if (ex is ConditionalExpression conditionalExpression)
+            {
+                return GetPropertyExpressionPath(ref name, conditionalExpression.IfFalse);
+            }
+
+            return name;
+        }
+    }
+}
