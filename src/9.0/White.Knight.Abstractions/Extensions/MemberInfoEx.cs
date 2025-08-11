c9bb73284be77957163286185b3d8dab5054d14c
Gabriel Palma <gubpalma@hotmail.com>
2025-08-11 14:49:27 +1000

    feat: exposed common extensions

---
+++ src/9.0/White.Knight.Abstractions/Extensions/MemberInfoEx.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace White.Knight.Abstractions.Extensions
+{
+    public static class MemberInfoEx
+    {
+        public static string GetMemberPropertyOrJsonAlias(this MemberInfo memberInfo)
+        {
+            return
+                memberInfo
+                    .GetCustomAttribute<JsonPropertyNameAttribute>()?
+                    .Name ??
+                memberInfo
+                    .Name;
+        }
+    }
+}
